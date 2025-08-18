7b99ee4996297167dc68edf76ef9f93a89213170
Uletin <dokterkoding111@gmail.com>
2025-08-18 23:33:25 +0800

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/ui/navbar";
 
-export const bebasNue = Bebas_Neue({
+const bebasNue = Bebas_Neue({
   subsets: ["latin"],
   weight: ["400"],
 });
@@ -16,9 +16,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${bebasNue.className} antialiased`}>
